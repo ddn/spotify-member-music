d25ecf85e5f8716e84e4802d70bd590d6d0698a4
David Dellanave <david@dellanave.com>
2013-03-01 20:52:57 -0600

    first commit tho

---
+++ README.md
@@ -0,0 +1,24 @@
+# Spotify Member Muic
+![Spotify Apps Tutorial home screen](https://github.com/spotify/apps-tutorial/raw/master/img/screenshot.png)
+
+This Spotify App pulls a list of playlists via a REST request, then randomly cycles through them playing a song from each.
+
+## Installation
+
+ 1. Sign up for a [developer account on Spotify](http://developer.spotify.com/en/spotify-apps-api/developer-signup/)
+ 2. Open Terminal, `mkdir ~/Spotify`
+ 3. `cd ~/Spotify`
+ 4. `git clone git://github.com/ddn/spotify-member-music.git`
+ 6. Download the [latest version of Spotify](http://spotify.com/download)
+ 7. Open Spotify and type "spotify:app:member-music" in the search bar (restart Spotify completely in case it doesn't find the App at first)
+
+## More information
+
+ * [API Reference](https://developer.spotify.com/technologies/apps/docs/)
+ * [Integration Guidelines](http://developer.spotify.com/download/spotify-apps-api/guidelines/)
+ * [Spotify-tagged questions on StackOverflow](http://stackoverflow.com/questions/tagged/spotify)
+ * [@SpotifyPlatform](https://twitter.com/#!/SpotifyPlatform) on Twitter
+
+Your REST API should return a JSON encoded array of a name and a URL such as:
+
+[{"uri":"http:\/\/open.spotify.com\/user\/ddn3d\/playlist\/4dnoJs5ZZgDK9B21pAJhpg","name":"David Dellanave"}]
---
+++ js/tutorial.js
@@ -0,0 +1,51 @@
+"use strict";
+window.onload = function() {
+    var sp = getSpotifyApi();
+    var models = sp.require('$api/models');
+
+    function htmlEscape(str) {
+        return String(str)
+                .replace(/&/g, '&amp;')
+                .replace(/"/g, '&quot;')
+                .replace(/'/g, '&#39;')
+                .replace(/</g, '&lt;')
+                .replace(/>/g, '&gt;');
+    }
+
+    function tabs() {
+        var args = models.application.arguments;
+        if (args) {
+            var lastArg = args[args.length - 1];
+            if (lastArg !== 'index' && lastArg !== 'tabs') {
+                return;
+           }
+       }
+
+        // compose file
+        var file = args.length == 1 ? (args[0] + '.html') : '/' + args.slice(0, args.length-1).join('/') + '.html';
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', file);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState != 4 || xhr.status != 200) return;
+
+            var wrapper = document.getElementById('wrapper');
+            wrapper.innerHTML = args[0] === 'index' ? '' : '<ul class="breadcrumb"><li><a href="spotify:app:spotify-gym-music:index">&laquo; Back to main page</a></li></ul>';
+            wrapper.innerHTML += xhr.responseText;
+
+            // search js snippets
+            var scripts = wrapper.querySelectorAll("script");
+            for (var i = 0; i < scripts.length; i++) {
+                if (scripts[i].getAttribute('type') == 'script/snippet') {
+                    var dataExecute = scripts[i].getAttribute('data-execute');
+                    if (!dataExecute || dataExecute != 'no') {
+                        eval(scripts[i].innerHTML);
+                    }
+                }
+            }
+
+        };
+        xhr.send(null);
+    }
+
+    models.application.observe(models.EVENT.ARGUMENTSCHANGED, tabs);
+};
